ef0bcc7387242141f8d7e646fb6c6719be6d57d0
Ryan Ripperger <rripperger@mail.csuchico.edu>
2017-04-25 19:55:11 -0700

    Completed Lab (4/25)

--- gallery.js
+++ gallery.js
@@ -1,6 +1,14 @@
-
-
+var chico = ["Bidwell Park", "Phantom Falls", "Flumes", "Willow Lake", "Campus", "Big Chico Creek"];
+var current = 0;
 function shuffleGallery()
 {
+  var randomValue = chico[Math.floor(Math.random()*chico.length)];
 
+  while (randomValue == current) {
+    var randomValue = chico[Math.floor(Math.random()*chico.length)];
+  }
+  current = randomValue;
+
+  //Prints the value of RandomValue
+  console.log(randomValue);
 }
